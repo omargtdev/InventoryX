f870b5b6e80ace7e3f9b8673c98fccbc6335e242
jaime chico loayza <jrchldev@gmail.com>
2023-09-30 14:36:18 -0500

    add utils

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./routes";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-green-500">Welcome to InventoryX!</h1>
-    </>
-  )
+	return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
