00ae43b0f9cb2ab01f24746e0c02d68510f932e9
Omar Gutierrez <omargtdev@gmail.com>
2023-09-30 13:27:09 -0500

    tailwind added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Welcome to InventoryX!</h1>
+      <h1 className="text-3xl font-bold underline text-green-500">Welcome to InventoryX!</h1>
     </>
   )
 }
