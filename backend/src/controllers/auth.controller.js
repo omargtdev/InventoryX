0c0daca0db71d1b2da4c80b2667f99688d48a871
Omar Gutierrez <omargtdev@gmail.com>
2023-09-30 22:05:24 -0500

    auth base structure added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+
+const getUserToken = (req, res) => {
+	return res.json({ token: "TOKEN" });
+}
+
+const getProfile = (req, res) => {
+
+}
+
+const updateProfile = (req, res) => {
+
+}
+
+export default {
+	getUserToken,
+	getProfile,
+	updateProfile
+}
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,1 +1,16 @@
-// TODO: Validate JWT
+const PATHS_TO_IGNORE = [
+	"/",
+	"/auth/login"
+]
+
+
+const validateJwt = (req, res, next) => {
+	if(PATHS_TO_IGNORE.includes(req.url))
+		return next();
+
+	// TODO: Add logic
+}
+
+export default {
+	validateJwt
+}
