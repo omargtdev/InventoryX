51f1453167a23cee858a6483d2a82f5190a5d2cb
Omar Gutierrez <omargtdev@gmail.com>
2023-10-01 21:51:11 -0500

    middleware: utils for clean empty strings added

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,14 +1,39 @@
+import statusCodes from "../config/status-codes.js";
+import jwtService from "../services/jwt.service.js";
+
 const PATHS_TO_IGNORE = [
 	"/",
 	"/auth/login"
 ]
 
+const messages = {
+	UNAUTHORIZED: "Unauthorized.",
+	INVALID_SCHEME: "Invalid authorization scheme."
+}
 
-const validateJwt = (req, res, next) => {
+const validateJwt = async (req, res, next) => {
 	if(PATHS_TO_IGNORE.includes(req.url))
 		return next();
 
-	// TODO: Add logic
+	const { authorization } = req.headers;
+	if(!authorization)
+		return res.status(statusCodes.UNAUTHORIZED).json({ message: messages.UNAUTHORIZED });
+
+	const [scheme, token] = authorization.split(" ");
+	
+	if(scheme !== "Bearer")
+		return res.status(statusCodes.UNAUTHORIZED).json({ message: messages.INVALID_SCHEME })
+
+	try {
+		await jwtService.validateToken(token);
+	} catch (errorMessage) {
+		return res.status(statusCodes.UNAUTHORIZED).json({ message: errorMessage });
+		
+	}
+	
+
+
+	next();
 }
 
 export default {
